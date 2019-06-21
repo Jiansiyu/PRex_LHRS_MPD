2a393a7ff345d361c590d4640f346786bef66a02
Siyu <jiansiyu@gmail.com>
2019-06-21 01:36:32 -0400

    save

--- replay/rootReader.C
+++ replay/rootReader.C
@@ -6,15 +6,24 @@
 #pragma cling load("libTreeSearch-GEM.so");
 #pragma cling load("../libprexCounting.so");
 
-int rootReader(TString fname){
+void rootReader(TString fname="test_20532.root"){
 	if(fname.IsNull()){
-		std::cout<<"Please inpu the file name"<<std::endl;
-		return 0;
+		std::cout<<"Please input the file name"<<std::endl;
 	}
 
 	TFile *fileio=TFile::Open(fname.Data());
 	TTree *PRex_GEM_tree;
 	fileio->GetObject("T",PRex_GEM_tree);
+	assert(fileio);
+	if(PRex_GEM_tree->IsZombie()){
+	}
 
-return 1;
+	// need to
+	double_t *fadc0;
+
+	PRex_GEM_tree->SetBranchAddress("prex.gems.y6.adc0",fadc0);
+	PRex_GEM_tree->GetEntry(25);
+
+	std::cout<<fadc0[0]<<fadc0[1]<<std::endl;
+
 }
